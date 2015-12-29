a23334f5465f602de4e4eea278694308005c57a8
Satyarth <satyarth934@gmail.com>
2015-12-30 00:10:58 +0400

    Using cache mem

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  get <- function() x
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  getInv <- function() inv
+  setInv <- function(inv2) inv <<- inv2
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  inv <- x$getInv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <<- solve(mat)
+  x$setInv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
